7584e73205937ca0f83e64a418cdb48bb3bd9b1f
milibefk <99682095+milibefk@users.noreply.github.com>
2022-04-19 14:04:00 -0700

    mili

--- specifier.c
+++ specifier.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
+​
 /**
  * get_specifier - finds the format func
  * @s: the format string
  *
  * Return: the number of bytes printed
  */
-int (*get_specifier(char *s))(va_list ap, params_t *params)
+	int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
 	specifier_t specifiers[] = {
 		{"c", print_char},
@@ -23,11 +23,9 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 		{"S", print_S},
 		{"r", print_rev},
 		{"R", print_rot13},
-		{NULL, NULL}
-	};
+		{NULL, NULL}};
 	int i = 0;
-
-	while (specifiers[i].specifier)
+	​while (specifiers[i].specifier)
 	{
 		if (*s == specifiers[i].specifier[0])
 		{
@@ -37,7 +35,7 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 	}
 	return (NULL);
 }
-
+​
 /**
  * get_print_func - finds the format func
  * @s: the format string
@@ -46,15 +44,14 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
  *
  * Return: the number of bytes printed
  */
-int get_print_func(char *s, va_list ap, params_t *params)
+	int
+	get_print_func(char *s, va_list ap, params_t *params)
 {
 	int (*f)(va_list, params_t *) = get_specifier(s);
-
-	if (f)
-		return (f(ap, params));
+	​ if (f) return (f(ap, params));
 	return (0);
 }
-
+​
 /**
  * get_flag - finds the flag func
  * @s: the format string
@@ -62,31 +59,31 @@ int get_print_func(char *s, va_list ap, params_t *params)
  *
  * Return: if flag was valid
  */
-int get_flag(char *s, params_t *params)
+	int
+	get_flag(char *s, params_t *params)
 {
 	int i = 0;
-
-	switch (*s)
+	​ switch (*s)
 	{
-		case '+':
-			i = params->plus_flag = 1;
-			break;
-		case ' ':
-			i = params->space_flag = 1;
-			break;
-		case '#':
-			i = params->hashtag_flag = 1;
-			break;
-		case '-':
-			i = params->minus_flag = 1;
-			break;
-		case '0':
-			i = params->zero_flag = 1;
-			break;
+	case '+':
+		i = params->plus_flag = 1;
+		break;
+	case ' ':
+		i = params->space_flag = 1;
+		break;
+	case '#':
+		i = params->hashtag_flag = 1;
+		break;
+	case '-':
+		i = params->minus_flag = 1;
+		break;
+	case '0':
+		i = params->zero_flag = 1;
+		break;
 	}
 	return (i);
 }
-
+​
 /**
  * get_modifier - finds the modifier func
  * @s: the format string
@@ -94,11 +91,11 @@ int get_flag(char *s, params_t *params)
  *
  * Return: if modifier was valid
  */
-int get_modifier(char *s, params_t *params)
+	int
+	get_modifier(char *s, params_t *params)
 {
 	int i = 0;
-
-	switch (*s)
+	​ switch (*s)
 	{
 	case 'h':
 		i = params->h_modifier = 1;
@@ -109,7 +106,7 @@ int get_modifier(char *s, params_t *params)
 	}
 	return (i);
 }
-
+​
 /**
  * get_width - gets the width from the format string
  * @s: the format string
@@ -118,12 +115,11 @@ int get_modifier(char *s, params_t *params)
  *
  * Return: new pointer
  */
-char *get_width(char *s, params_t *params, va_list ap)
-/* should this function use char **s and modify the pointer? */
+	char *
+	get_width(char *s, params_t *params, va_list ap)
 {
 	int d = 0;
-
-	if (*s == '*')
+	​ if (*s == '*')
 	{
 		d = va_arg(ap, int);
 		s++;
